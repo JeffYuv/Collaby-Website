3440b00d95e314e157464f6694b7534d2e653756
jeff <you@example.com>
2020-10-27 21:40:10 -0400

    added nav to register/login pages

--- login.php
+++ login.php
@@ -5,14 +5,34 @@
 <head>
   <title>Login</title>
   <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" type="text/css" href="styles.css" />
 </head>
 
 <body>
 
+  <header>
+    <nav>
+      <a href="index.html">
+        <img src="images/CollabyLogo.png" alt="Collaby Logo" width="60px" height="50px" />
+      </a>
+      <h4 class="logotitle">Collaby</h4>
+      <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="discover.html">Discover</a></li>
+        <li><a href="home.php">My Page</a></li>
+        <li><a href="features.html">Features</a></li>
+        <li><a href="help.html">Help</a></li>
+        <li><a href="aboutus.html">About</a></li>
+        <li><a href="status.html">Status</a></li>
+      </ul>
+      <a href="home.php"><button class="login login1">Login</button></a>
+    </nav>
+  </header>
+
 
   <section class="img">
 
-    <div class="box">
+    <div class="formlog">
       <div class="header">
         <img src="images/CollabyLogo.png" alt="Collaby Logo" width="100px" height="90px" />
         <h2>Login</h2>
